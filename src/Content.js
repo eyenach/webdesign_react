1a4b2996003a184731b5a236efd6e594e20a2985
Aalums <59070194@it.kmitl.ac.th>
2019-04-15 15:52:03 +0700

    เพิ่มคอลัมสำหรับใส่ประเภทอาหาร
    อาจจะต้องดึงข้อมูลจาก DB มาใส่ ไม่ก็ hardcode

--- src/Content.js
+++ src/Content.js
@@ -5,10 +5,21 @@ import pizza from './pizza.jpg';
 class Content extends Component {
     render() {
         return (
-            <div className="padding">
-                <img src={pizza} className="display center-fit" />
+            <div>
+                <div className="padding">
+                    <img src={pizza} className="display center-fit"/>
 
-                <h3 className="white">Category</h3>
+                    <h3 className="white">Category</h3>
+                </div>
+
+                <div className="container">
+                    <div className="row">
+                        <div className="column">1</div>
+                        <div className="column">2</div>
+                        <div className="column">3</div>
+                        <div className="column">4</div>
+                    </div>
+                </div>
             </div>
         );
     }
